5ec5a6132bd0f29179928ae88916c70f55b404b9
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-02 11:36:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData(name, email){
+  let configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: name,
+    email: email
+  })
+};
+return fetch('http://localhost:3000/users',configurationObject)
+.then(function(response){
+  return response.json();
+})
+.then(function(data){
+  document.body.innerHTML=data["id"];
+})
+.catch(function(error){
+  document.body.innerHTML=error.message
+})
+}
